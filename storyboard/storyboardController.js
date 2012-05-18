baf94cb04ecfda36d394fba785dd769ce95a2158
Marco Liceti <marco.liceti@gmail.com>
2012-05-18 21:03:28 +0200

    Added storyboardController stub.

---
+++ storyboard/storyboardController.js
@@ -0,0 +1,47 @@
+StoryboardController = function(listener) {
+	this.listener = listener;
+};
+
+/* UC1. */
+
+StoryboardController.prototype.addActor = function(description) {
+	// body...
+};
+
+/* UC2. */
+
+StoryboardController.prototype.addEvent = function(description) {
+	// body...
+};
+
+/* UC3. */
+
+StoryboardController.prototype.startAddSegment = function(description) {
+	// body...
+};
+
+StoryboardController.prototype.setActorForNewSegment = function(actorId) {
+	// body...
+};
+
+StoryboardController.prototype.setFromEventForNewSegment = function(eventId) {
+	// body...
+};
+
+StoryboardController.prototype.setToEventForNewSegment = function(eventId) {
+	// body...
+};
+
+StoryboardController.prototype.setDurationForNewSegment = function(duration) {
+	// body...
+};
+
+StoryboardController.prototype.addSegment = function() {
+	// body...
+};
+
+/* UC4. */
+
+StoryboardController.prototype.processStoryboard = function() {
+	// body...
+};
