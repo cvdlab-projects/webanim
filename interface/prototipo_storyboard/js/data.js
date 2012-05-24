20ecba732e915713c7908aeb7bba889aaaa3cd86
Flavio <xanderwallace@hotmail.com>
2012-05-24 15:37:17 +0200

    prototipo1 storyboard ui

---
+++ interface/prototipo_storyboard/js/data.js
@@ -0,0 +1,16 @@
+var timelineData = [
+	{
+		id: 1, name: "Actor 1", series: [
+			{ name: "s1", start: 3, end: 10 },
+			{ name: "s2", start: 23, end: 50 }
+		]
+	}, 
+	{
+		id: 2, name: "Actor 2", series: [
+			{ name: "s1", start: 10, end: 11 },
+			{ name: "s2", start: 20, end: 30 },
+			{ name: "s3", start: 33, end: 90 }
+		]
+
+	}
+];
